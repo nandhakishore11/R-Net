4f0b8299959a3a5066306acea61092881a3982f4
Nandhakishore Srinivasan <nandhakishore11@gmail.com>
2019-04-12 09:52:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ dev_file = os.path.join(home, "data", "squad", "dev-v1.1.json")
 test_file = os.path.join(home, "data", "squad", "dev-v1.1.json")
 glove_word_file = os.path.join(home, "data", "glove", "glove.840B.300d.txt")
 
-target_dir = "data"
+target_dir = "/content/data"
 log_dir = "log/event"
 save_dir = "log/model"
 answer_dir = "log/answer"
